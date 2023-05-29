72a838794d130a8b53c0d7c7838163f16026f4a7
melissa espitaleta <m.espitaleta1@gmail.com>
2023-05-29 16:16:01 -0500

    Improving validation on date fields

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const DAY = 1000*60*60*24;
 const MONTH = 1000*60*60*24*30.5; 
 const YEAR = 1000*60*60*24*365; // or 31557600000 
 const DEFAULT_SPAN_TEXT = "- -";
-const CELEBRATE = "Happy Birthday you turned"
-const ERROR_STATE = {
-    DAY_ERROR_TEXT: "Must be a valid day",
-    MONTH_ERROR_TEXT: "Must be a valid Month",
-    YEAR_ERROR_TEXT: "Must be in the past", 
+const CELEBRATE = "Happy Birthday you turned";
+const ERROR_MESSAGE = {
+    DAY: "Must be a valid day",
+    MONTH: "Must be a valid Month",
+    YEAR: "Must be in the past", 
     VALID_YEAR: "Must be a valid year",
-    VALID_DATE: "Insert a valid date",
+    VALID_DATE: "Must be valid date",
+    EMPTY_FIELDS: "This field is required",
 };
 
 const INVALID_INPUT = {
+    DAY: 32,
     MONTH: 13, 
     VALUE: 0,
-}
+};
 
 
 /* NODE ELEMENTS */ 
@@ -34,9 +36,8 @@ const monthSpan = document.getElementById("months_text");
 const yearSpan = document.getElementById("years_text");
 
 
-
 /* VARIABLES */
-let inputDate;
+let birthDate;
 let daysInMonth;
 let inputValues = {
     day: "",
@@ -44,98 +45,175 @@ let inputValues = {
     year: "",
 };
 
+let errors = {
+    day: {
+        input: dayInput,
+        label: dayLabel,
+        text: "",
+    },
+    month: {
+        input: monthInput,
+        label: monthLabel,
+        text: "",
+    },
+    year: {
+        input: yearInput,
+        label: yearLabel,
+        text: "",
+    },
+};
+
+
 const inputTextValue = (e) => {
     const value = e.target.value;
     const inputName = e.target.id; 
     inputValues[inputName] = value;
-}
+};
 
-const showErrorMessage = (inputElem, labelElem, textError) => {
+const showErrorState = (inputElem, labelElem, textError) => {
     const errorMessage = document.createElement("span"); 
 
-    inputElem.classList.add("warning");
-    labelElem.classList.add("error_text_state");        
+    inputElem.classList.add("error-warning");
+    labelElem.classList.add("error-state_title");        
     labelElem.append(errorMessage);
     errorMessage.innerHTML = textError;  
-    errorMessage.classList.add("error_state");
+    errorMessage.classList.add("error-state_text");
+};
+
+const removeErrorState = (key, keyInput, keyLabel) => {
+
+    if(key == "day") {
+        dayInput.addEventListener("change", ()=>{
+            keyInput.classList.remove("error-warning");
+            keyLabel.classList.remove("error-state_title");
+
+            spanElem = document.getElementsByClassName("error-state_text");
+            spanElem[0]?.remove();
+
+            monthInput.classList.remove("error-warning");
+            monthLabel.classList.remove("error-state_title"); 
+            yearInput.classList.remove("error-warning");
+            yearLabel.classList.remove("error-state_title");
+        }); 
+    }
+
+
+    if(key == "month") {
+        monthInput.addEventListener("change", ()=>{
+            keyInput.classList.remove("error-warning");
+            keyLabel.classList.remove("error-state_title");
+
+            spanElem = document.getElementsByClassName("error-state_text");
+            spanElem[0]?.remove();
+
+            dayInput.classList.remove("error-warning");
+            dayLabel.classList.remove("error-state_title");
+            yearInput.classList.remove("error-warning");
+            yearLabel.classList.remove("error-state_title"); 
+        }); 
+    }
+
+    if(key == "year") {
+        yearInput.addEventListener("change", ()=>{
+            keyInput.classList.remove("error-warning");
+            keyLabel.classList.remove("error-state_title"); 
+            
+            spanError = document.getElementsByClassName("error-state_text");
+            spanElem[0]?.remove();
+
+            dayInput.classList.remove("error-warning");
+            dayLabel.classList.remove("error-state_title"); 
+            monthInput.classList.remove("error-warning");
+            monthLabel.classList.remove("error-state_title"); 
+        }); 
+    }
 }
 
-const getDaysInMonth = (yearValue, monthValue) => {
-    return new Date(yearValue, monthValue, 0).getDate(); 
-}
+const getDaysInMonth = (yearValue, monthValue) => new Date(yearValue, monthValue, 0).getDate(); 
 
 const calculateAge = () => {
     const currentDay = new Date();
-    inputDate = new Date(`${inputValues.month}/${inputValues.day}/${inputValues.year}`);
-
-    const getInputDay = inputDate.getDate();
-    const getInputMonth = inputDate.getMonth(); 
-    const getInputYear = inputDate.getFullYear();
-
-    const getCurrentDay = currentDay.getDate();
-    const getCurrentMonth = currentDay.getMonth();
-    const getCurrentYear = currentDay.getFullYear(); 
-
-    daysInMonth = getDaysInMonth(getInputYear, inputValues.month); 
-    console.log(daysInMonth, "28");
-
-
-    /* DATE VALIDATIONS */
-    //Empty day validation
-    if(!inputValues.day || !inputValues.month || !inputValues.year){
-        showErrorMessage(dayInput, dayLabel, ERROR_STATE.VALID_DATE); 
-        showErrorMessage(monthInput, monthLabel, ""); 
-        showErrorMessage(yearInput, yearLabel, ""); 
-    }
+    birthDate = new Date(`${inputValues.month}/${inputValues.day}/${inputValues.year}`);
     
-    //Numeric Dates validations
-    if(inputValues.day && inputValues.month && inputValues.year ){
+    const getCurrentYear = currentDay.getFullYear();
+    const getInputYear = birthDate.getFullYear();
+    daysInMonth = getDaysInMonth(getInputYear, inputValues.month); 
 
-        //if day is zero or greater than 31 is an invalid day per year
-        if(Number(inputValues.day) == INVALID_INPUT.VALUE || Number(inputValues.day) > daysInMonth){
-            showErrorMessage(dayInput, dayLabel, ERROR_STATE.DAY_ERROR_TEXT); 
+    Object.keys(inputValues).map((key) => {
+        const currentValue = inputValues[key];
+        //if all the fields are empty
+        if(!currentValue) {
+            errors[key].text = ERROR_MESSAGE.EMPTY_FIELDS;
+            showErrorState(errors[key].input, errors[key].label, errors[key].text);
+            removeErrorState(key,errors[key].input, errors[key].label);
+        } else {
+            //if one or all the fields are zero
+            if(Number(currentValue) == INVALID_INPUT.VALUE) {
+                errors.day.text = ERROR_MESSAGE.DAY;
+                errors.month.text = ERROR_MESSAGE.MONTH;
+                errors.year.text = ERROR_MESSAGE.VALID_YEAR;
+                showErrorState(errors[key].input, errors[key].label, errors[key].text);
+                removeErrorState(key,errors[key].input, errors[key].label);
+            }
+
+            //Validating the day field: 
+            if(key == "day" && Number(currentValue) > daysInMonth ) {
+                //if the day fields is greater than the limit of day per month eg: 30/feb/2020
+                errors[key].text = ERROR_MESSAGE.VALID_DATE;
+                showErrorState(errors[key].input, errors[key].label, errors[key].text); 
+                showErrorState(monthInput, monthLabel, "");
+                showErrorState(yearInput, yearLabel, "");
+
+                removeErrorState(key,errors[key].input, errors[key].label);
+
+            } else if(key == "day" && Number(currentValue) >= INVALID_INPUT.DAY) {
+                //if the day fields is greater than 31
+                errors[key].text = ERROR_MESSAGE.DAY;
+                showErrorState(errors[key].input, errors[key].label, errors[key].text); 
+                showErrorState(monthInput, monthLabel, "");
+                showErrorState(yearInput, yearLabel, "");
+
+                removeErrorState(key,errors[key].input, errors[key].label);
+            }
+
+            //if the month field is greater or equal to 13 
+            if(key == "month" && Number(currentValue) >= INVALID_INPUT.MONTH) {
+                errors[key].text = ERROR_MESSAGE.MONTH;
+                showErrorState(errors[key].input, errors[key].label, errors[key].text);
+                showErrorState(dayInput, dayLabel, "");
+                showErrorState(yearInput, yearLabel, "");
+
+                removeErrorState(key,errors[key].input, errors[key].label);
+            }
+
+            //if the year field is greater or equal to the actual year
+            if(key == "year" && Number(currentValue) >= getCurrentYear) {
+                errors[key].text = ERROR_MESSAGE.YEAR;
+                showErrorState(errors[key].input, errors[key].label, errors[key].text);
+                showErrorState(dayInput, dayLabel, "");
+                showErrorState(monthInput, monthLabel, "");
+
+                removeErrorState(key,errors[key].input, errors[key].label);
+            }
         }
+    }); 
 
-        //if month is zero or greater than 12 is an invalid month
-        if(Number(inputValues.month) == INVALID_INPUT.VALUE || Number(inputValues.month) >= INVALID_INPUT.MONTH) {
-            showErrorMessage(monthInput, monthLabel, ERROR_STATE.MONTH_ERROR_TEXT);
-        } 
 
-        //if year is zero is an invalid year
-        if(Number(inputValues.year) == INVALID_INPUT.VALUE) {
-            showErrorMessage(yearInput, yearLabel, ERROR_STATE.VALID_YEAR); 
-        }
-
-        //if the getInputYear is greater than the getCurrentYear is an invalid year
-        if(getInputYear >= getCurrentYear) {
-            showErrorMessage(yearInput, yearLabel, ERROR_STATE.YEAR_ERROR_TEXT);
-        }
-    };
-     /* END OF DATE VALIDATIONS */
-
+    
 
     /* CURRENT AGE CALC */
-    const age = currentDay - inputDate;
+    const age = currentDay - birthDate;
     const days = Math.floor((age / 1000) / DAY); 
     const months = Math.floor((age % YEAR) / MONTH);
     const years = Math.floor(age / YEAR);
      /* END OF CURRENT AGE CALC */
 
     //render a default value if the year, month and day are not calculated or are invalid
-    if(days && months && years) {
-        yearSpan.innerHTML = String(years); 
-        monthSpan.innerHTML = String(months); 
-        daySpan.innerHTML = String(days); 
-    } else {
+    if(!days && !months && !years) {
         yearSpan.innerHTML = DEFAULT_SPAN_TEXT; 
         monthSpan.innerHTML = DEFAULT_SPAN_TEXT; 
         daySpan.innerHTML = DEFAULT_SPAN_TEXT;
-    }
-
-
-    //if the day and month is the same of the currenteDay day and month is your birthday!
-    if(getCurrentDay == getInputDay && getCurrentMonth == getInputMonth) {            
-        alert(`${CELEBRATE} ${years}!!!`); 
+    } else {
         yearSpan.innerHTML = String(years); 
         monthSpan.innerHTML = String(months); 
         daySpan.innerHTML = String(days); 
